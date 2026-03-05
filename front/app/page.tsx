125a6d49f144bbe9aaa0d7a249f8f0e65afd7a70
Valentin <valentin.roquejofre@epitech.eu>
2026-03-05 17:16:26 +0100

    feat: add the sidebar with a searchbar in every pages

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -10,29 +10,6 @@ export default function Home() {
         <main className="flex-1">
           <h1>Page principale</h1>
 
-          <Link href="/profile">
-            <button>Aller à la page de profile</button>
-          </Link>
-
-          <Link href="/event">
-            <button>Aller à la page d'evenement</button>
-          </Link>
-
-          <Link href="/calendar">
-            <button>Aller à la page de calendrier</button>
-          </Link>
-
-          <Link href="/file">
-            <button>Aller à la page de fichier</button>
-          </Link>
-
-          <Link href="/adminUser">
-            <button>Aller à la page d'admin pour les profiles</button>
-          </Link>
-
-          <Link href="/adminEvent">
-            <button>Aller à la page d'admin pour les events</button>
-          </Link>
         </main>
       </div>
     </SidebarProvider>
