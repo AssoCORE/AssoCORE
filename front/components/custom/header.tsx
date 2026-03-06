15ed337f88a0949b4a7c0aac870399d5adda14ec
Lilian Bazantay <lilian.bazantay@epitech.eu>
2026-03-06 11:50:04 +0000

    feat: create a wrapper around MUI breadcrumb
    fix: move breadcrumb to correct spot

---
+++ front/components/custom/header.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+type headerProps = {
+  link: string;
+};
+
+type HeaderProps = {
+  link: string;
+};
+
+function BreadCrumbElement({ link }: HeaderProps) {
+  const paths = link.split("/");
+
+  function getPathTo(segment: string) {
+    const parts = usePathname().split("/").filter(Boolean);
+    segment = segment.slice(2);
+    const index = parts.indexOf(segment);
+
+    if (parts.join("/") == segment) return "/" + segment;
+
+    if (index === -1) return "/";
+
+    return "/" + parts.slice(0, index + 1).join("/");
+  }
+
+  return (
+    <div style={{ display: "flex", alignItems: "center", gap: "4px" }}>
+      {paths.map((seg, i) => {
+        const href = "/" + paths.slice(0, i + 1).join("/");
+        const url = getPathTo(href);
+
+        if (href === "/")
+          return (
+            <React.Fragment key={i}>
+              <button>
+                <BreadcrumbItem>
+                  <BreadcrumbLink href={url}>home</BreadcrumbLink>
+                </BreadcrumbItem>
+              </button>
+              {i < paths.length - 1 && <BreadcrumbSeparator />}
+            </React.Fragment>
+          );
+
+        return (
+          <React.Fragment key={i}>
+            <button>
+              <BreadcrumbItem>
+                <BreadcrumbLink href={url}>{seg}</BreadcrumbLink>
+              </BreadcrumbItem>
+            </button>
+            {i < paths.length - 1 && <BreadcrumbSeparator />}
+          </React.Fragment>
+        );
+      })}
+    </div>
+  );
+}
+
+export default function header() {
+  const pathName = usePathname();
+
+  return (
+    <Breadcrumb>
+      <BreadcrumbList>
+        <BreadCrumbElement link={pathName} />
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
+}
