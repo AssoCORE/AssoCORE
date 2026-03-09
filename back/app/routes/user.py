db47ac2ef3c722f3a33e2e6371c56c2c77b3297d
Antoine Quillet <antoine.quillet@epitech.eu>
2026-03-09 11:33:29 +0100

    feat(back/routes): add nextcloud-related routes with data

--- back/app/routes/user.py
+++ back/app/routes/user.py
@@ -128,12 +128,51 @@ async def get_ns_cloud():
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+@router.get("/cloud/?dir={dir}", description="send an address toward NextCloud's file system")
+async def get_ns_specific_folder(dir):
+    try:
+        return JSONResponse(content={"Response":"OK"})
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@router.get("/cloud/?dir={dir}&file={file}", description="send an address towards specific file into Nextcloud")
+async def get_ns__specific_file(dir, file):
+    try:
+        return JSONResponse(content={"Response":"OK"})
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@router.get("/cloud/search/{filename}", description="send an address towards Nextcloud's search file system")
+async def get_ns__specific_file(dir, file):
+    try:
+        return JSONResponse(content={"Response":"OK"})
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@router.get("/file/{id}", description="send an address toward Nextcloud's file viewer")
+async def get_ns_file_viewer(id):
+    try:
+        return JSONResponse(content={"Response":"OK"})
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+
 @router.get("/calendar/", description="send an address toward NextCloud's calendar system")
 async def get_ns_calendar():
     try:
         return JSONResponse(content={"Response":"OK"})
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+@router.get("/calendar/view={viewType}", description="send an address toward NextCloud's calendar system with specific view type")
+async def get_ns_specific_calendar(viewType):
+    try:
+        return JSONResponse(content={"Response":"OK"})
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
 
 @router.get("/contact/", description="send an address toward NextCloud's contact system")
 async def get_ns_contacts():
@@ -142,9 +181,23 @@ async def get_ns_contacts():
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+@router.get("/contact/{contact_id}", description="send an address toward NextCloud's contact system with a specific target")
+async def get_ns_specific_contacts(contact_id):
+    try:
+        return JSONResponse(content={"Response":"OK"})
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
 
 @router.get("/notes/", description="send an address toward NextCloud's notes system")
 async def get_ns_notes():
+    try:
+        return JSONResponse(content={"Response":"OK"})
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@router.get("/notes/{note_id}", description="send an address toward NextCloud's notes system with a specific target")
+async def get_ns_specific_note(note_id):
     try:
         return JSONResponse(content={"Response":"OK"})
     except Exception as e:
