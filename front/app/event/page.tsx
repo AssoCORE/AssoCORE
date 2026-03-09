e1941a5ed6c3d59473cba3e787d6a613d8de4291
Lilian Bazantay <lilian.bazantay@epitech.eu>
2026-03-09 09:28:14 +0000

    feat: implement breadcrumb into header

--- front/app/event/page.tsx
+++ front/app/event/page.tsx
@@ -1,15 +1,16 @@
 import Link from "next/link";
 import { AppSidebar } from "@/components/custom/sidebar";
 import { SidebarProvider } from "@/components/ui/sidebar";
+import Header from "@/components/custom/header";
 
 export default function Home() {
   return (
     <SidebarProvider>
       <div className="flex">
         <AppSidebar />
+        <Header />
         <main className="flex-1">
           <h1>Page Event</h1>
-
         </main>
       </div>
     </SidebarProvider>
