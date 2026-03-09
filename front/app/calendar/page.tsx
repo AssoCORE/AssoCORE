6929b2e721bd5505d3803fe0baaa7832807710c7
Lilian Bazantay <lilian.bazantay@epitech.eu>
2026-03-09 09:28:14 +0000

    feat: implement breadcrumb into header

--- front/app/calendar/page.tsx
+++ front/app/calendar/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { AppSidebar } from "@/components/custom/sidebar";
 import { SidebarProvider } from "@/components/ui/sidebar";
+import Header from "@/components/custom/header";
 
 export default function Home() {
   return (
@@ -8,8 +9,8 @@ export default function Home() {
       <div className="flex">
         <AppSidebar />
         <main className="flex-1">
+          <Header />
           <h1>Page Calendar</h1>
-
         </main>
       </div>
     </SidebarProvider>
